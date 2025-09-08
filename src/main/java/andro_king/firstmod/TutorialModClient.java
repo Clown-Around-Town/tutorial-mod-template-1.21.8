bd61d43e9a48d489efff0ae4187f0b5e0f988632
Andro <ankng010@gmail.com>
2025-09-08 20:07:07 +0200

    Basics of the mod

---
+++ src/main/java/andro_king/firstmod/TutorialModClient.java
@@ -0,0 +1,7 @@
+package andro_king.firstmod;
+import net.fabricmc.api.ClientModInitializer;
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
