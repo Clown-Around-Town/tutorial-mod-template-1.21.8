398dc98b52b610f8bff168eb71e96e4bdad2b74f
Andro <ankng010@gmail.com>
2025-09-09 16:22:36 +0200

    added new blocks, added ability to break and craft blocks

--- src/main/java/andro_king/firstmod/block/ModBlocks.java
+++ src/main/java/andro_king/firstmod/block/ModBlocks.java
@@ -3,10 +3,9 @@ package andro_king.firstmod.block;
 
 import andro_king.firstmod.TutorialMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.MapColor;
-import net.minecraft.enchantment.Enchantment;
+import net.minecraft.block.*;
+import net.minecraft.block.enums.NoteBlockInstrument;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroups;
@@ -16,12 +15,52 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.stateprovider.PillarBlockStateProvider;
+
+import java.util.function.Function;
 
 public class ModBlocks {
-    public static final Block SCULK_PILLAR = registerBlock("sculk_pillar", AbstractBlock.Settings.create()
-            .strength(0.4F, 0.6F).requiresTool().sounds(BlockSoundGroup.SCULK_CATALYST).mapColor(MapColor.OFF_WHITE));
+    //registration of unique blocks themselves
+    //TODO: make sculk_pillar drop xp if mined without silktouch
+    public static final Block SCULK_PILLAR = registerPillarBlock("sculk_pillar", AbstractBlock.Settings.create()
+        .strength(0.75F, 2.5F)
+        .requiresTool()
+        .sounds(BlockSoundGroup.SCULK_CATALYST)
+        .mapColor(MapColor.PALE_YELLOW)
+    );
+    public static final Block SCULK_BRICKS = registerBlock("sculk_bricks", AbstractBlock.Settings.create()
+        .strength(1.5F, 3.0F)
+        .requiresTool()
+        .sounds(BlockSoundGroup.SCULK_CATALYST)
+        .mapColor(MapColor.PALE_YELLOW)
+    );
+    public static final Block CRACKED_SCULK_BRICKS = registerBlock("cracked_sculk_bricks", AbstractBlock.Settings.create()
+            .strength(1.3F, 2.5F)
+            .requiresTool()
+            .sounds(BlockSoundGroup.SCULK_CATALYST)
+            .mapColor(MapColor.PALE_YELLOW)
+    );
+    public static final Block SILENT_SCULK = registerBlock("silent_sculk", AbstractBlock.Settings.create()
+            .strength(0.15F)
+            .requiresTool()
+            .sounds(BlockSoundGroup.SCULK)
+            .mapColor(MapColor.BLACK)
+    );
+    /*
+    public static final Block SCULK_SPIKE = registerSpike("sculk_spike", AbstractBlock.Settings.create()
+            .mapColor(MapColor.PALE_YELLOW)
+            .solid()
+            .nonOpaque()
+            .sounds(BlockSoundGroup.SCULK_CATALYST)
+            .strength(1.5F, 3.0F)
+            .dynamicBounds()
+            .offset(AbstractBlock.OffsetType.XZ)
+            .pistonBehavior(PistonBehavior.DESTROY)
+            .solidBlock(Blocks::never)
+    );
+    */
 
-
+    //different kinds of block registration
     private static Block registerBlock(String name, AbstractBlock.Settings blockSettings) {
         RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TutorialMod.MOD_ID, name));
         Block block = new Block(blockSettings.registryKey(key));
@@ -29,6 +68,21 @@ public class ModBlocks {
         return Registry.register(Registries.BLOCK, key, block);
     }
 
+    private static Block registerPillarBlock(String name, AbstractBlock.Settings blockSettings) {
+        RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TutorialMod.MOD_ID, name));
+        Block block = new PillarBlock(blockSettings.registryKey(key));
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, key, block);
+    }
+
+    private static Block registerSpike(String name, AbstractBlock.Settings blockSettings) {
+        RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TutorialMod.MOD_ID, name));
+        Block block = new PointedDripstoneBlock(blockSettings.registryKey(key));
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, key, block);
+    }
+
+    //general registration
     private static void registerBlockItem(String name, Block block) {
         RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID, name));
         BlockItem item = new BlockItem(block, new Item.Settings().registryKey(key));
@@ -40,5 +94,7 @@ public class ModBlocks {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(
                 fabricItemGroupEntries -> fabricItemGroupEntries.add(ModBlocks.SCULK_PILLAR));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(
+                fabricItemGroupEntries -> fabricItemGroupEntries.add(ModBlocks.SCULK_BRICKS));
     }
 }
